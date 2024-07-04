72ff11ea1e8020910523d1897daa4e88786007b8
NicolayDodonov <95965202+NicolayDodonov@users.noreply.github.com>
2024-07-04 15:53:45 +0400

    Fix simulation packege import

--- internal/simulation/simulation.go
+++ internal/simulation/simulation.go
@@ -2,7 +2,7 @@ package simulation
 
 import (
 	"fmt"
-	"geneticAutomat/internal/world"
+	"geneticAutomat/internal/model"
 	"math/rand"
 )
 
@@ -14,15 +14,15 @@ func Run() {
 	//TODO: init console print
 
 	//TODO: create world
-	var model world.World = world.CreateWorld(64, 64)
-	model.GenerateWalls()
-	model.GenerateFood(2)
+	var world model.World = model.CreateWorld(64, 64)
+	world.GenerateWalls()
+	world.GenerateFood(2)
 
 	//TODO: create start population
-	arrayEntity := make([]world.Entity, population)
+	arrayEntity := make([]model.Entity, population)
 	for i := 0; i < population; i++ {
-		arrayEntity[i] = world.CreateEntity(rand.Intn(model.Height), rand.Intn(model.Width),
-			world.RandomDNA())
+		arrayEntity[i] = model.CreateEntity(rand.Intn(world.Height), rand.Intn(world.Width),
+			model.RandomDNA())
 	}
 	//TODO: create goroutine of simulation
 	for age := 0; age < 120; age++ {
@@ -30,15 +30,15 @@ func Run() {
 		for i := 0; i < population; i++ {
 			if arrayEntity[i].Hp > 0 {
 				countLive++
-				world.RunDNA(&arrayEntity[i], &model)
+				model.RunDNA(&arrayEntity[i], &world)
 			} else if !(arrayEntity[i].Hp == -1) {
-				model.UpdateEntityCell(arrayEntity[i].Coordinates, nil)
-				model.SetPoisonCell(arrayEntity[i].Coordinates, 10)
+				world.UpdateEntityCell(arrayEntity[i].Coordinates, nil)
+				world.SetPoisonCell(arrayEntity[i].Coordinates, 10)
 				arrayEntity[i].Hp = -1
 			}
 		}
 		if age%10 == 0 {
-			model.CountOfEntity = countLive
+			world.CountOfEntity = countLive
 			fmt.Print(countLive)
 			fmt.Println(" age: ", age)
 		}
